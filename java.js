23295580d41e52ecb1a2917c41da6ace759ce8b7
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-02-11 15:28:40 -0300

    integrado com o html do Lucas

--- java.js
+++ java.js
@@ -1,79 +1,222 @@
-let arrMovies = [
-    "spirited+away",
-    "the+little+prince",
-    "chicken+run",
-    "coco",
-    "arrival",
-    "interstellar",
-    "lucy",
-    "blade+runner",
-    "logan",
-    "fantastic+beasts+and+where+to+find+them",
-    "aquaman",
-    "the+old+guard",
-    "yesterday",
-    "a+star+is+born",
-    "her",
-    "carol"
-];
+/*************************FILMES QUE IRAO APARECER NO CARROSSEL************************************** */
+let carrouselMovies = [
+    {
+        name:  "the+little+prince",
+        gender: "Anime/Desenho"
+    },
 
-let arrGender = [
-    "Anime/Desenho",
-    "Sci-Fi",
-    "Aventura",
-    "Romance"
-];
+    {
+        name: "chicken+run",
+        gender: "Anime/Desenho"
+    },
 
-let k = 0;
+    {
+        name: "coco",
+        gender: "Anime/Desenho"
+    },
 
-for(let i = 0; i < arrMovies.length; i += 4){
-    let gender = document.createElement("p");
-    gender.textContent = arrGender[k];
-    gender.style.fontWeight = "bold";
-    gender.style.textAlign = "center";
-    for(let j = i; j < i + 4; j++){
-        let request = new XMLHttpRequest();
-        request.open("GET", `http://www.omdbapi.com/?t=${arrMovies[j]}&plot=full&apikey=ff8d5bd3`);
-        
-        request.addEventListener("load", ()=>{
-            console.log(JSON.parse(request.responseText));
-            let img = document.createElement("img");
-            img.addEventListener("click", () =>{
-                if(document.getElementById("movieInfo")){
-                    document.getElementsByTagName("body")[0].removeChild(document.getElementById("movieInfo"));
-                }
-                let div = document.createElement("div");
-                div.setAttribute("id", "movieInfo");
-                document.getElementsByTagName("body")[0].appendChild(div);
+    {
+        name: "arrival",
+        gender: "Ficção Científica"
+    },
 
-                let title = document.createElement("p");
-                title.textContent = "Title: " + JSON.parse(request.responseText).Title;
-                title.style.fontWeight = "bold";
-                let runTime = document.createElement("p");
-                runTime.textContent = "Run Time: " + JSON.parse(request.responseText).Runtime;
-                runTime.style.fontWeight = "bold";
-                let imdbRating = document.createElement("p");
-                imdbRating.textContent = "IMBD: " + JSON.parse(request.responseText).imdbRating;
-                let plot = document.createElement("p");
-                plot.textContent = "Plot: " + JSON.parse(request.responseText).Plot;
-                plot.style.fontWeight = "bold";
+    {
+        name: "interstellar",
+        gender: "Ficção Científica"
+    },
 
-                div.appendChild(title);
-                div.appendChild(runTime);
-                div.appendChild(imdbRating);
-                div.appendChild(plot);
-            });
-            img.src = JSON.parse(request.responseText).Poster;
-            document.getElementById("p" + i).appendChild(gender);
-            document.getElementById(i).appendChild(img);
-            
-        }, false);
-        
-        request.send();
-        
+    {
+        name: "lucy",
+        gender: "Ficção Científica"
+    },
+
+    {
+        name: "blade+runner",
+        gender: "Aventura"
+    },
+
+    {
+        name: "logan",
+        gender: "Ação"
+    },
+
+    {
+        name: "fantastic+beasts+and+where+to+find+them",
+        gender: "Aventura"
+    },
+
+    {
+        name: "aquaman",
+        gender: "Aventura"
+    },
+
+    {
+        name: "the+old+guard",
+        gender: "Ação"
+    },
+
+    {
+        name: "yesterday",
+        gender: "Romance"
+    },
+
+    {
+        name: "a+star+is+born",
+        gender: "Romance"
+    },
+
+    {
+        name: "her",
+        gender: "Romance"
+    },
+
+    {
+        name: "carol",
+        gender: "Romance"
     }
-    
-    k++;
-};
+
+];
 
 
+for (let i = 0; i < 15; i++) {
+    let request = new XMLHttpRequest();
+    request.open("GET", `http://www.omdbapi.com/?t=${carrouselMovies[i].name}&plot=full&apikey=ff8d5bd3`);
+
+    request.addEventListener("load", () => {
+        let divCarousel = document.createElement("div");
+        divCarousel.setAttribute("class", "carousel-item");
+        let img = document.createElement("img");
+        img.src = JSON.parse(request.responseText).Poster;
+        img.setAttribute("class", "d-block w-100 imagem-carrossel");
+        img.setAttribute("alt", "...");
+        let innerDiv = document.createElement("div");
+        innerDiv.setAttribute("class", "carousel-caption d-none d-md-block h-25");
+        divCarousel.appendChild(img);
+        divCarousel.appendChild(innerDiv);
+        let h1 = document.createElement("h1");
+        h1.setAttribute("class", "fonte-titulo");
+        h1.textContent = carrouselMovies[i].gender;
+        innerDiv.appendChild(h1);
+        document.getElementsByClassName("carousel-inner")[0].appendChild(divCarousel);
+        /*img.addEventListener("click", () =>{
+            if(document.getElementById("movieInfo")){
+                document.getElementsByTagName("body")[0].removeChild(document.getElementById("movieInfo"));
+            }
+            let div = document.createElement("div");
+            div.setAttribute("class", "movieInfo");
+            document.getElementsByTagName("body")[0].appendChild(div);
+
+            let title = document.createElement("p");
+            title.textContent = "Title: " + JSON.parse(request.responseText).Title;
+            title.style.fontWeight = "bold";
+            let runTime = document.createElement("p");
+            runTime.textContent = "Run Time: " + JSON.parse(request.responseText).Runtime;
+            runTime.style.fontWeight = "bold";
+            let imdbRating = document.createElement("p");
+            imdbRating.textContent = "IMBD: " + JSON.parse(request.responseText).imdbRating;
+            imdbRating.style.fontWeight = "bold";
+            let plot = document.createElement("p");
+            plot.textContent = "Plot: " + JSON.parse(request.responseText).Plot;
+            plot.style.fontWeight = "bold";
+
+            div.appendChild(title);
+            div.appendChild(runTime);
+            div.appendChild(imdbRating);
+            div.appendChild(plot);
+        });*/
+
+    }, false);
+
+    request.send();
+}
+
+
+/**************************FIM DOS FILMES QUE IRÃO APARECER NO CARROSSEL****************************************************** */
+
+
+/*************************COMEÇO DOS FILMES QUE IRÃO APARECER NA PAGINA INICIAL******************************************************* */
+let bannerMovies = [
+    "murder+on+the+orient+express",
+    "the+hunger+games",
+    "home+alone",
+    "the+book+thief",
+    "madagascar",
+    "inside+out",
+    "hidden+figures",
+    "2001:+a+space+odyssey",
+    "call+me+by+your+name",
+    "back+to+the+future",
+    "the+breakfast+club",
+    "brokeback+mountain"
+];
+
+for(let i = 0; i < 12; i++){
+    let request = new XMLHttpRequest();
+    request.open("GET", `http://www.omdbapi.com/?t=${bannerMovies[i]}&plot=short&apikey=ff8d5bd3`);
+
+    request.addEventListener("load", () => {
+        let article = document.createElement("article");
+        article.setAttribute("class", "card borda-cor-especial card-largura p-0 m-4 col-12 col-md-4");
+        document.getElementsByClassName("row justify-content-center")[0].appendChild(article);
+        
+        let img = document.createElement("img");
+        img.setAttribute("class", "card-img-top card-posicao-imagem");
+        img.setAttribute("alt", "...");
+        img.src = JSON.parse(request.responseText).Poster;
+        article.appendChild(img);
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "card-body");
+        div.setAttribute("id", i);
+        article.appendChild(div);
+
+        let h5 = document.createElement("h5");
+        h5.setAttribute("class", "card-title");
+        h5.textContent = JSON.parse(request.responseText).Title;
+        div.appendChild(h5);
+
+        let p = document.createElement("p");
+        p.setAttribute("class", "card-text");
+        p.textContent = JSON.parse(request.responseText).Production;
+        div.appendChild(p);
+
+        let a = document.createElement("a");
+        a.setAttribute("class", "btn botao-cor-especial");
+        a.textContent = "Detalhes";
+        div.appendChild(a);
+
+        a.addEventListener("click", () => {
+            if(document.getElementById("info" + i)){
+                document.getElementById(i).removeChild(document.getElementById("info" + i));
+            }
+            let div = document.createElement("div");
+            div.setAttribute("id", "info" + i);
+            document.getElementById(i).appendChild(div);
+            let title = document.createElement("p");
+            title.textContent = "Title: " + JSON.parse(request.responseText).Title;
+            title.style.fontWeight = "bold";
+            let runTime = document.createElement("p");
+            runTime.textContent = "Run Time: " + JSON.parse(request.responseText).Runtime;
+            runTime.style.fontWeight = "bold";
+            let imdbRating = document.createElement("p");
+            imdbRating.textContent = "IMBD: " + JSON.parse(request.responseText).imdbRating;
+            imdbRating.style.fontWeight = "bold";
+            let plot = document.createElement("p");
+            plot.textContent = "Plot: " + JSON.parse(request.responseText).Plot;
+            plot.style.fontWeight = "bold";
+            div.appendChild(title);
+            div.appendChild(runTime);
+            div.appendChild(imdbRating);
+            div.appendChild(plot);
+        });
+
+    }, false);
+
+    request.send();
+}
+
+
+
+
+
+/*************************FIM DOS FILMES QUE IRÃO APARECER NA PAGINA INICIAL************************************************************** */
